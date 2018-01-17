8e7e2d585d3cbff2ce8dd673507337b791f8ab09
Helton Denis <heltondenis.sistemas@gmail.com>
2018-01-16 22:26:20 -0200

    Desenvolvimento do navbar

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php require 'pages/header.php'; ?>
+
+	<!-- JUMBOTRON -->
+	<div class="container-fluid">
+		<div class="jumbotron">
+			<h3><strong>Nós temos hoje 10 anúncios.</strong></h3>
+			<p><h4>E mais de 950 usuários cadastrados.</h4></p>
+		</div>
+		<div class="row">
+			<div class="col-sm-3">
+				<h3>Pesquisa Avançada</h3>
+			</div>
+			<div class="col-sm-9">
+				<h3>Últimos Anúncios</h3>
+			</div>
+		</div>
+	</div>
+	<!-- END JUMBOTRON -->
+
+<?php require 'pages/footer.php'; ?>
---
+++ pages/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Classified</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script src="assets/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
+	<script src="assets/js/script.js" type="text/javascript" charset="utf-8" async defer></script>
+	<script src="assets/js/jquery-3.2.1.min.js" type="text/javascript" charset="utf-8" async defer></script>
+</head>
+<body>
+
+	<!-- NAVBAR -->
+	<nav class="navbar navbar-inverse bg-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="./">
+					Classified
+				</a>
+			</div>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="">Cadastra-se</a></li>
+				<li><a href="">Login</a></li>
+			</ul>
+		</div>
+	</nav>
+	<!-- END NAVBAR -->
